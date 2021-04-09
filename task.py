6bd89282e3691d2f9c9236919e193ca6694eb460
salomequoy <>
2021-04-09 10:59:13 +0000

    Task 2 commit

--- task.py
+++ task.py
@@ -1,26 +1,44 @@
-#
-#Banking simulator. Write a code in python that simulates the banking system. 
-#The program should:
-# - be able to create new banks
-# - store client information in banks
-# - allow for cash input and withdrawal
-# - allow for money transfer from client to client
-#If you can think of any other features, you can add them.
-#This code shoud be runnable with 'python task.py'.
-#You don't need to use user input, just show me in the script that the structure of your code works.
-#If you have spare time you can implement: Command Line Interface, some kind of data storage, or even multiprocessing.
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+class bank :
+  def __init__(self,name,bic,location,clients) :
+    self.name=name
+    self.bic=bic
+    self.location=location
+    self.clients=clients
+
+class client :
+  def __init__(self,first_name,last_name,date_of_birth,bank_account,password) :
+    self.first_name=first_name
+    self.last_name=last_name
+    self.date_of_birth=date_of_birth
+    self.bank_account=bank_account
+    self.password=password
+
+class bank_account :
+  def __init__(self,account_number,amount,withdrawal_limit,withdrawal_limit_status) :
+    self.account_number=account_number
+    self.amount=amount
+    self.withdrawal_limit=withdrawal_limit
+    self.withdrawal_limit_status=withdrawal_limit_status
+
+def transfer(account_number_giving,account_number_receiving,transfer_amount): 
+  bank_account_giving= #Bank account with account_number_giving
+  bank_account_receiving=#Bank account with account_number_receiving
+  bank_account_giving.amount=bank_account_giving.amount-transfer_amount
+  bank_account_receiving.amount=bank_account_giving.amount-transfer_amount
+
+def withdrawal(account_number,withdrawal_amount):
+  bank_account= #Bank account with this account_number
+  if (bank_account.withdrawal_limit>bank_account.withdrawal_limit_status+withdrawal_amount) :
+    bank_account.amount=bank_account.amount-withdrawal_amount
+
+
+if __name__ = "__main__" :
+  bank_account_1=bank_account(21,3000,400,20)
+  bank_account_2=bank_account(26,100,30,0)
+  transfer(21,26,500)
+  withdrawal(21,100)
+
+
+
+
+
